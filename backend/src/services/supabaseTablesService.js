fae01542f1731eeefa56969e1f0c806869b3238d
JasperRosales <23-19300@g.batstate-u.edu.ph>
2026-07-15 15:35:40 +0800

    fixing the things here

--- backend/src/services/supabaseTablesService.js
+++ backend/src/services/supabaseTablesService.js
@@ -1,11 +1,6 @@
 import { getSupabaseClient } from '../config/supabaseClient.js'
-<<<<<<< HEAD
-export const gasnetTables = [
-
-=======
 
 export const gasnetTables = [
->>>>>>> 2f805d250f222a61991c72257f02c55e875a24b2
   'branches',
   'staff',
   'products',
@@ -22,10 +17,6 @@ async function fetchTableRows(client, tableName) {
     throw new Error(`Failed to fetch "${tableName}": ${error.message}`)
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 2f805d250f222a61991c72257f02c55e875a24b2
   return data
 }
 
@@ -39,7 +30,4 @@ export async function getAllPosTablesData(client = getSupabaseClient()) {
 
   return Object.fromEntries(tableEntries)
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f805d250f222a61991c72257f02c55e875a24b2
